1da0f9beba8cc848ecb2c410dc9bd66b9ccd8201
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-09-19 05:04:16 +0000

    Describe your changes here

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,67 +2,64 @@
 
 pragma solidity 0.8.19;
 
-
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
+contract Raffle is VRFConsumerBaseV2Plus {
+    // CUSTOM ERRORS
+    error Raffle_InsufficientFee();
+    error NotEnoughTimePassed();
 
+    // variable declaration
+    uint256 private immutable i_entranceFee;
+    uint256 public s_lotteryStartTime;
+    uint256 private immutable i_intervalTime;
+    address payable[] public raffleFunders;
 
+    // client struct variable declaration
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subId;
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
+    // Constructor
+    constructor(uint256 entranceFee, uint256 intervalTime, uint256 startTime,bytes32 gaslane,uint256 subId,uint32 callbackGasLimit,address VRFCOORDINATOR)
+    VRFConsumerBaseV2Plus(VRFCOORDINATOR)
+     {
+        i_entranceFee = entranceFee;
+        i_intervalTime = intervalTime;
+        s_lotteryStartTime = block.timestamp;
+        i_keyHash = gaslane;
+i_subId = subId;
+i_callbackGasLimit = callbackGasLimit;
 
-contract Raffle is VRFConsumerBaseV2Plus{
+    }
 
-// CUSTOM ERRORS
-error Raffle_InsufficientFee();
-error NotEnoughTimePassed();
-
-
-// variable declaration
-uint256 private immutable i_entranceFee;
-uint256 public s_lotteryStartTime;
-uint256 private immutable i_intervalTime;
-address payable[] public raffleFunders;
-
-
-// Constructor
-constructor(uint256 entranceFee,uint256 intervalTime,uint256 startTime){
-i_entranceFee = entranceFee;
-i_intervalTime = intervalTime;
-s_lotteryStartTime = block.timestamp;
-}
-
-event raffleLogEntry(address indexed players);
-
-
+    event raffleLogEntry(address indexed players);
 
     function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_InsufficientFee();
+        }
 
- if(msg.value<i_entranceFee){
-    revert Raffle_InsufficientFee();
- }
+        raffleFunders.push(payable(msg.sender));
 
- raffleFunders.push(payable(msg.sender));
+        emit raffleLogEntry(msg.sender);
+    }
 
-emit raffleLogEntry(msg.sender);
+    function pickWinner() public {
+        if ((block.timestamp - s_lotteryStartTime) < i_intervalTime) {
+            revert NotEnoughTimePassed();
+        }
+
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({});
 
     }
 
-
-
-
-    function pickWinner() public{
-if((block.timestamp -s_lotteryStartTime)<i_intervalTime){
-    revert NotEnoughTimePassed();
-}
-
-
-
-    }
-
-    function checkEntranceFee() public view returns(uint256){
+    function checkEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
-
 }
 
 /*
