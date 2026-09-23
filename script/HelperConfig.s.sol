b8aafc25750e023638b655d351f14c316dee2e9a
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-09-23 22:02:16 +0530

    FOUNDRY

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -27,7 +27,7 @@ struct NetworkConfig{
     address VRFCOORDINATOR;
      bytes32 gaslane; 
      uint256 subId; 
-     uint32 callbackGasLimit
+     uint32 callbackGasLimit;
      
 }
 
@@ -56,7 +56,7 @@ config = NetworkConfig({
     intervalTime: 30,
 VRFCOORDINATOR:0x271682DEB8C4E0901D1a1550aD2e64D568E69909,
 gaslane:0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445,
-subId:,
+subId:0,
 callbackGasLimit:5000
 });
 return config;
@@ -72,7 +72,7 @@ function getSepolia() public pure returns(NetworkConfig memory){
     intervalTime: 30,
 VRFCOORDINATOR:0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
 gaslane:0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
-subId:,
+subId:0,
 callbackGasLimit:5000
 });
 return config;
@@ -98,7 +98,7 @@ config = NetworkConfig({
     intervalTime: 30,
 VRFCOORDINATOR:address(mocking),
 gaslane:0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
-subId:,
+subId:0,
 callbackGasLimit:5000
 });
 return config;
