f2e363a1f466dda4012fc3fa37a63fd86f702907
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-09-23 19:56:47 +0530

    FOUNDRY

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,13 +2,16 @@
 
 pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
-
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 
 abstract contract DataConstants{
     uint256 constant ETH_MAINNET_CHAINID = 1;
     uint256 constant ETH_SEPOLIA_CHAINID =11155111 ;
     uint256 constant ANVIL_CHAINID = 31337;
+    uint96 constant BASE_FEE =  0.5 ether;
+    uint96 constant GAS_FEE= 1e15;
+     int256 constant WEI_PER_LINK = 1e9;
 }
 
 
@@ -61,6 +64,33 @@ return config;
 
 
 
+function getAnvil() public returns(NetworkConfig memory){
+
+if(config.VRFCOORDINATOR != address(0)){
+    return config;
+}
+
+vm.startBroadcast();
+
+VRFCoordinatorV2_5Mock mocking = new VRFCoordinatorV2_5Mock(BASE_FEE,GAS_FEE,WEI_PER_LINK);
+
+vm.stopBroadcast();
+
+
+config = NetworkConfig({
+    entranceFee:1 ether,
+    intervalTime: 30,
+VRFCOORDINATOR:address(mocking),
+gaslane:0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+subId:,
+callbackGasLimit:5000
+});
+return config;
+
+
+}
+
+
 
 
 
@@ -70,3 +100,32 @@ return config;
 
 
 }
+
+
+/*
+LEARNINGS
+
+Sepolia / Mainnet (getSepoliaEthConfig)
+No if check needed.The contracts already exist live on-chain. The function simply returns hardcoded constant values from memory, costing zero gas and creating no side effects.  
+
+ Anvil / Local Chain (getOrCreateAnvilEthConfig)if check is required: 
+ if (localNetworkConfig.vrfCoordinator != address(0))   
+ Why: On local chains, the Chainlink contracts do not exist yet, so we must deploy a mock contract on the fly using new VRFCoordinatorV2_5Mock(...).  
+ Purpose: The if check acts as a singleton cache. It ensures the mock is deployed only once. Subsequent calls return the saved address from localNetworkConfig, preventing duplicate mock deployments that would break tests
+
+
+
+
+Scenario WITHOUT the if check (The Bug)
+
+
+Imagine you write a test suite in Foundry with 2 separate tests: Test A and Test B.
+
+
+
+
+Test A runs:It calls helperConfig.getConfig().getOrCreateAnvilEthConfig() runs, deploys Mock #1 at address 0x111....   Your script creates a Chainlink Subscription on Mock #1 (0x111...) and adds your Raffle contract as a consumer.Test A passes!
+Test B runs immediately after:It calls helperConfig.getConfig().getOrCreateAnvilEthConfig() runs again, deploying Mock #2 at address 0x222...!   Your Raffle contract is deployed, but it tries to request random words from Mock #2 (0x222...).BOOM! Test B fails.
+ Why? Because subscription funding and consumer registration happened on Mock #1, but Raffle is trying to call Mock #2 which knows nothing about your subscription!
+
+ */
