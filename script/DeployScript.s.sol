e5fb78d40c9aba09661ee6467cfec364f0be32a6
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-09-23 22:34:52 +0530

    FOUNDRY

--- script/DeployScript.s.sol
+++ script/DeployScript.s.sol
@@ -3,11 +3,31 @@
 pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {HelperConfig} from ./HelperConfig.s.sol;
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {Raffle} from "../src/Raffle.sol";
 
 
 contract Deploycontract is Script {
     function run() public {}
 
-    function contractLogic() public {}
+    function contractLogic() public returns(HelperConfig,Raffle) {
+HelperConfig deployConfig = new HelperConfig();
+HelperConfig.NetworkConfig memory raffleConfig = deployConfig.getConfig();
+
+vm.startBroadcast();
+Raffle raffle = new Raffle(
+raffleConfig.entranceFee,
+raffleConfig.intervalTime
+raffleConfig.VRFCOORDINATOR
+raffleConfig.gaslane
+raffleConfig.subId
+raffleConfig.callbackGasLimit
+
+);
+vm.stopBroadcast();
+
+return (deployConfig , raffle);
+
+    }
 }
+
