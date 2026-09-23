53fcaed28615005a2bf89c2624b99422bdcfcc5a
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-09-23 11:48:47 +0530

    FOUNDRY

---
+++ script/DeployScript.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+contract Deploycontract is Script{
+    function run() public{}
+
+    function contractLogic() public{
+        
+    }
+}
